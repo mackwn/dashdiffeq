5f695c6a092af2f9ee76dfbabcd47dc6b2ee8b0b
Spencer McKone <smckon1@outlook.com>
2020-05-08 09:08:38 -0500

    add second tab

--- app.py
+++ app.py
@@ -27,112 +27,122 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
         }
     ),
 
-    html.Div(children='q = -k\u2207\u00B2',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-    }),
+
+    dcc.Tabs([
+        dcc.Tab(label='Steady-State 2D', children=[
+
+        html.Div(children='q = -k\u2207\u00B2',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+        }),
 
 
-    html.Div([
-    
-    
-    
-    
-    
-    ##Div for graph
         html.Div([
-            dcc.Graph(
-                id='elliptic-graph',
-                figure={
-                    'data': [
-                        {'z': ugrid, 'type': 'contour', 'name': 'SF','transpose':True,
-                        'x':xgrid[:,0],'y':ygrid[0,:]
-                        },
-                    ],
-                    'layout': {
-                        'plot_bgcolor': colors['background'],
-                        'paper_bgcolor': colors['background'],
-                        'font': {
-                            'color': colors['text']
+        
+        ##Div for graph
+            html.Div([
+                dcc.Graph(
+                    id='elliptic-graph',
+                    figure={
+                        'data': [
+                            {'z': ugrid, 'type': 'contour', 'name': 'SF','transpose':True,
+                            'x':xgrid[:,0],'y':ygrid[0,:]
+                            },
+                        ],
+                        'layout': {
+                            'plot_bgcolor': colors['background'],
+                            'paper_bgcolor': colors['background'],
+                            'font': {
+                                'color': colors['text']
+                            }
                         }
                     }
-                }
+                )
+            ],style={'width': '40%', 'display': 'inline-block', 'padding': '0 20'}),
+
+        
+            #Div for sliders
+            html.Div([
+
+            html.Div(['u(x=0,y=y): ',html.Span(id='uox-slider-output-container'),' K']),    
+
+            dcc.Slider( #u(x=0,y=y)
+                id='uox-slider',
+                min=200,
+                max=800,
+                value=500,
+                step=25
+                #marks={temp: '%s K'%temp for temp in range(300,750,50)},
+                #step=None
+            ),
+            html.Div(['u(x=f,y=y): ',html.Span(id='ufx-slider-output-container'),' K']),
+            dcc.Slider( #u(x=f,y=y)
+                id='ufx-slider',
+                min=200,
+                max=800,
+                value=500,
+                step=25,
+
+                #marks={temp: '%s K'%temp for temp in range(300,750,50)},
+                #step=None
+            ),
+            html.Div(['u(x=x,y=0): ',html.Span(id='uoy-slider-output-container'),' K']),
+            dcc.Slider( #u(x=x,y=0)
+                id='uoy-slider',
+                min=200,
+                max=800,
+                value=500,
+                step=25
+                #marks={temp: '%s K'%temp for temp in range(300,750,50)},
+                #step=None
+            ), 
+            html.Div(['u(x=x,y=f): ',html.Span(id='ufy-slider-output-container'),' K']),
+            dcc.Slider( #u(x=x,y=f)
+                id='ufy-slider',
+                min=200,
+                max=800,
+                value=500,
+                step=25
+                #marks={temp: '%s K'%temp for temp in range(300,750,50)},
+                #step=None
+            ),
+            html.Div(['q: ',html.Span(id='heatflux-slider-output-container'),' mW/m\u00B2']),
+            dcc.Slider(
+                id='heatflux-slider',
+                min=0,
+                max=1,
+                step=.05,
+                value=.5
+                #marks={flux: '%s mW/m\u00B2'%int(round(flux*1000,0)) for flux in np.linspace(0,1.1,10)},
+                #step=None
+            ), 
+            html.Div(['k: ',html.Span(id='conductivity-slider-output-container'),' mW/K']),
+            dcc.Slider(
+                id='conductivity-slider',
+                min=.00001,
+                max=.00101,
+                value=.00051,
+                step=.00005
+                #marks={cond: '%s mW/K'%round(cond*1000,2) for cond in np.linspace(.00001,.001,10)},
+                #marks={'label': str(round(cond,5)) for cond in np.linspace(.00001,.001,10)},
+                #step=None
             )
-        ],style={'width': '40%', 'display': 'inline-block', 'padding': '0 20'}),
-
-    
-        #Div for sliders
-        html.Div([
-
-        html.Div(['u(x=0,y=y): ',html.Span(id='uox-slider-output-container'),' K']),    
-
-        dcc.Slider( #u(x=0,y=y)
-            id='uox-slider',
-            min=200,
-            max=800,
-            value=500,
-            step=25
-            #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-            #step=None
-        ),
-        html.Div(['u(x=f,y=y): ',html.Span(id='ufx-slider-output-container'),' K']),
-        dcc.Slider( #u(x=f,y=y)
-            id='ufx-slider',
-            min=200,
-            max=800,
-            value=500,
-            step=25,
-
-            #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-            #step=None
-        ),
-        html.Div(['u(x=x,y=0): ',html.Span(id='uoy-slider-output-container'),' K']),
-        dcc.Slider( #u(x=x,y=0)
-            id='uoy-slider',
-            min=200,
-            max=800,
-            value=500,
-            step=25
-            #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-            #step=None
-        ), 
-        html.Div(['u(x=x,y=f): ',html.Span(id='ufy-slider-output-container'),' K']),
-        dcc.Slider( #u(x=x,y=f)
-            id='ufy-slider',
-            min=200,
-            max=800,
-            value=500,
-            step=25
-            #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-            #step=None
-        ),
-        html.Div(['q: ',html.Span(id='heatflux-slider-output-container'),' mW/m\u00B2']),
-        dcc.Slider(
-            id='heatflux-slider',
-            min=0,
-            max=1,
-            step=.05,
-            value=.5
-            #marks={flux: '%s mW/m\u00B2'%int(round(flux*1000,0)) for flux in np.linspace(0,1.1,10)},
-            #step=None
-        ), 
-        html.Div(['k: ',html.Span(id='conductivity-slider-output-container'),' mW/K']),
-        dcc.Slider(
-            id='conductivity-slider',
-            min=.00001,
-            max=.00101,
-            value=.00051,
-            step=.00005
-            #marks={cond: '%s mW/K'%round(cond*1000,2) for cond in np.linspace(.00001,.001,10)},
-            #marks={'label': str(round(cond,5)) for cond in np.linspace(.00001,.001,10)},
-            #step=None
-        )
+            ],
+            style={'width': '40%', 'padding': '20px 20px 20px 20px', 'display': 'inline-block'}) #end slider div
         ],
-        style={'width': '40%', 'padding': '20px 20px 20px 20px', 'display': 'inline-block'}) #end slider div
-    ],
-    style={'padding': '20px 20px 20px 20px','color': colors['text']}
-    ),
+        style={'padding': '20px 20px 20px 20px','color': colors['text']}
+        )
+        ]),
+        dcc.Tab(label='Transient 1D', children=[
+
+            html.Div(children='dq/dt = -k\u2207\u00B2',
+                style={
+                    'textAlign': 'center',
+                    'color': colors['text']
+            })
+        ])
+    ])  
 
 ])
 
