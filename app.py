c61ab19fbfe7e1eee6e5c6b800aa75d3ca795ad8
Spencer McKone <smckon1@outlook.com>
2020-05-09 17:21:28 -0500

    get template working

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 from elliptic2d import elliptic2dsolve
 from dash.dependencies import Input, Output 
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/dZVMbK.css']
 
@@ -28,7 +29,7 @@ slider_labels = {#label:[min,max,symbol,unit]
     'conductivity':[.00001,.00101,'k','mW/K']
 }
 
-def render_slider(label,vmin,vmax,symbol,units): ###rewrite this to only do the slider formating, and then use list comprehension instead of for loop
+def render_slider(label,vmin,vmax,symbol,units): 
 
     output = dbc.Row([
             dbc.Col(
@@ -52,27 +53,24 @@ def render_slider(label,vmin,vmax,symbol,units): ###rewrite this to only do the 
         ])
     return output 
 
-def render_contourplot(xgrid,ygrid,ugrid):
-    output = dcc.Graph(
-        id='elliptic-graph',
-        figure={
-            'data': [
-                {'z': ugrid, 'type': 'surface', 'name': 'SF','transpose':True,
-                'x':xgrid[:,0],'y':ygrid[0,:]
-                },
-            ],
-            'layout': {
-                # 'plot_bgcolor': colors['background'],
-                # 'paper_bgcolor': colors['background'],
-                # 'font': {
-                #     'color': colors['text']
-                # },
-                'template':'plotly_dark',
-                'margin': dict(l=10, r=10, b=10, t=10)
-            }
-        }
+def render_surfaceplot(xgrid,ygrid,ugrid):
+
+    figure = go.Figure(
+        data = go.Surface(
+            x=xgrid,y=ygrid,z=ugrid,
+            colorbar=dict(
+                title='Temp. (K)'
+                )
+            ),
+        layout = go.Layout(
+            template="plotly_dark",
+            margin= dict(l=40, r=10, b=40, t=10),
+            scene=dict(xaxis_title="X (m)",
+            yaxis_title="Y (m)",
+            zaxis_title="Temp. (K)")
+            )
     )
-    return output
+    return figure
 
 app.layout = dbc.Container([
     dbc.Row([
@@ -85,7 +83,9 @@ app.layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col( #Graph 
-            [render_contourplot(xgrid=xgrid,ygrid=ygrid,ugrid=ugrid)],
+            [dcc.Graph(
+                figure=render_surfaceplot(xgrid=xgrid,ygrid=ygrid,ugrid=ugrid),id='elliptic-graph'
+                )],
             md=6,sm=12
         ),
         dbc.Col([dbc.Row(dbc.Col([html.H5('Boundary Conditions & Parameters')],width=12))]+[
@@ -96,138 +96,6 @@ app.layout = dbc.Container([
         )
     ])
 ])
-
-
-
-# app.layout = html.Div(children=[
-#     html.H1(
-#         children='Interactive 2D Heat Equation',
-#         style={
-#             #'textAlign': 'center',
-#             #'color': colors['text']
-#         }
-#     ),
-
-
-
-
-#     dcc.Tabs([
-#         dcc.Tab(label='Steady-State 2D', children=[
-
-#         html.Div(children='q = -k\u2207\u00B2',
-#             style={
-#                 'textAlign': 'center'#,
-#                 #'color': colors['text']
-#         }),
-
-
-#         html.Div([
-        
-#         ##Div for graph
-#             html.Div([
-#                 dcc.Graph(
-#                     id='elliptic-graph',
-#                     figure={
-#                         'data': [
-#                             {'z': ugrid, 'type': 'contour', 'name': 'SF','transpose':True,
-#                             'x':xgrid[:,0],'y':ygrid[0,:]
-#                             },
-#                         ],
-#                         'layout': {
-#                             'plot_bgcolor': colors['background'],
-#                             'paper_bgcolor': colors['background'],
-#                             'font': {
-#                                 'color': colors['text']
-#                             }
-#                         }
-#                     }
-#                 )
-#             ],style={'width': '40%', 'display': 'inline-block', 'padding': '0 20'}),
-
-        
-#             #Div for sliders
-#             html.Div([
-
-#             html.Div(['u(x=0,y=y): ',html.Span(id='uox-slider-output-container'),' K']),    
-
-#             dcc.Slider( #u(x=0,y=y)
-#                 id='uox-slider',
-#                 min=200,
-#                 max=800,
-#                 value=500,
-#                 step=25
-#                 #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-#                 #step=None
-#             ),
-#             html.Div(['u(x=f,y=y): ',html.Span(id='ufx-slider-output-container'),' K']),
-#             dcc.Slider( #u(x=f,y=y)
-#                 id='ufx-slider',
-#                 min=200,
-#                 max=800,
-#                 value=500,
-#                 step=25,
-
-#                 #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-#                 #step=None
-#             ),
-#             html.Div(['u(x=x,y=0): ',html.Span(id='uoy-slider-output-container'),' K']),
-#             dcc.Slider( #u(x=x,y=0)
-#                 id='uoy-slider',
-#                 min=200,
-#                 max=800,
-#                 value=500,
-#                 step=25
-#                 #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-#                 #step=None
-#             ), 
-#             html.Div(['u(x=x,y=f): ',html.Span(id='ufy-slider-output-container'),' K']),
-#             dcc.Slider( #u(x=x,y=f)
-#                 id='ufy-slider',
-#                 min=200,
-#                 max=800,
-#                 value=500,
-#                 step=25
-#                 #marks={temp: '%s K'%temp for temp in range(300,750,50)},
-#                 #step=None
-#             ),
-#             html.Div(['q: ',html.Span(id='heatflux-slider-output-container'),' mW/m\u00B2']),
-#             dcc.Slider(
-#                 id='heatflux-slider',
-#                 min=0,
-#                 max=1,
-#                 step=.05,
-#                 value=.5
-#                 #marks={flux: '%s mW/m\u00B2'%int(round(flux*1000,0)) for flux in np.linspace(0,1.1,10)},
-#                 #step=None
-#             ), 
-#             html.Div(['k: ',html.Span(id='conductivity-slider-output-container'),' mW/K']),
-#             dcc.Slider(
-#                 id='conductivity-slider',
-#                 min=.00001,
-#                 max=.00101,
-#                 value=.00051,
-#                 step=.00005
-#                 #marks={cond: '%s mW/K'%round(cond*1000,2) for cond in np.linspace(.00001,.001,10)},
-#                 #marks={'label': str(round(cond,5)) for cond in np.linspace(.00001,.001,10)},
-#                 #step=None
-#             )
-#             ],
-#             style={'width': '40%', 'padding': '20px 20px 20px 20px', 'display': 'inline-block'}) #end slider div
-#         ],
-#         style={'padding': '20px 20px 20px 20px','color': colors['text']}
-#         )
-#         ]),
-#         dcc.Tab(label='Transient 1D', children=[
-
-#             html.Div(children='dq/dt = -k\u2207\u00B2',
-#                 style={
-#                     'textAlign': 'center'#,
-#                    # 'color': colors['text']
-#             })
-#         ])
-#     ])  
-
-# ])
 
 
 for slider in ['uox-slider','ufx-slider','uoy-slider','ufy-slider','heatflux-slider','conductivity-slider']:
@@ -250,24 +118,8 @@ for slider in ['uox-slider','ufx-slider','uoy-slider','ufy-slider','heatflux-sli
 )
 def update_figure(uox,ufx,uoy,ufy,flux,conduc):
     xgrid,ygrid,ugrid = elliptic2dsolve(3,4,[uoy,ufy],[uox,ufx],flux,conduc,10,10)
-    return {
-        'data': [
-                    {'z': ugrid, 'type': 'surface', 'name': 'Temperature Surface','transpose':True,
-                    'x':xgrid[:,0],'y':ygrid[0,:]
-                    }
-        ],
-        'layout': {
-            # 'plot_bgcolor': colors['background'],
-            # 'paper_bgcolor': colors['background'],
-            # 'font': {
-            #     'color': colors['text']
-            # },
-            'margin': dict(l=10, r=10, b=10, t=10),
-            'template':"plotly_dark"
 
-        }
-        
-    }
+    return render_surfaceplot(xgrid,ygrid,ugrid)
     
 
 
